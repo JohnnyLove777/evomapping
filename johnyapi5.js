2ef604af5d88ad7498bb7ccd1fc1cadeeb63f40d
JohnnyLove777 <47362296+JohnnyLove777@users.noreply.github.com>
2024-05-16 14:46:06 -0300

    post teste 09

--- johnyapi5.js
+++ johnyapi5.js
@@ -20,46 +20,49 @@ if (!fs.existsSync('media')) {
 // Lista de eventos suportados
 const events = ['messages-upsert'];
 
-const downloadAndDecryptMedia = async (url, mimetype, fileName, mediaKey) => {
-  const extension = mime.extension(mimetype);
-  const filePath = path.join('media', `${fileName}.${extension}`);
-
-  // Download the encrypted file
-  const response = await axios({
-    url,
-    method: 'GET',
-    responseType: 'arraybuffer',
-  });
-
-  // Descrypt the media
-  const encryptedMedia = Buffer.from(response.data, 'binary');
-  const mediaKeyExpanded = crypto.createHash('sha256').update(mediaKey).digest();
-  const iv = mediaKeyExpanded.slice(0, 16);
-  const cipherKey = mediaKeyExpanded.slice(16, 48);
-
-  const decipher = crypto.createDecipheriv('aes-256-cbc', cipherKey, iv);
-  const decryptedMedia = Buffer.concat([decipher.update(encryptedMedia), decipher.final()]);
-
-  fs.writeFileSync(filePath, decryptedMedia);
-};
-
 // Função para imprimir mensagens específicas e baixar mídias
 const printMessageDetails = async (data) => {
+  const downloadAndDecryptMedia = async (url, mimetype, fileName, mediaKey, type) => {
+    const extension = mime.extension(mimetype);
+    const filePath = path.join('media', `${fileName}.${extension}`);
+
+    // Download the encrypted file
+    const response = await axios({
+      url,
+      method: 'GET',
+      responseType: 'arraybuffer',
+    });
+
+    // Expand the media key
+    const mediaKeyBuffer = Buffer.from(mediaKey, 'base64');
+    const infoStr = `WhatsApp ${type.charAt(0).toUpperCase() + type.slice(1)} Keys`;
+    const expandedMediaKey = crypto.createHmac('sha256', mediaKeyBuffer).update(infoStr).digest();
+    const iv = expandedMediaKey.slice(0, 16);
+    const cipherKey = expandedMediaKey.slice(16, 48);
+
+    // Decrypt the media
+    const decipher = crypto.createDecipheriv('aes-256-cbc', cipherKey, iv);
+    const encryptedMedia = Buffer.from(response.data);
+    const decryptedMedia = Buffer.concat([decipher.update(encryptedMedia), decipher.final()]);
+
+    fs.writeFileSync(filePath, decryptedMedia);
+  };
+
   if (data.message.audioMessage) {
     console.log("Audio Message:", JSON.stringify(data.message.audioMessage, null, 2));
-    await downloadAndDecryptMedia(data.message.audioMessage.url, data.message.audioMessage.mimetype, data.key.id, data.message.audioMessage.mediaKey);
+    await downloadAndDecryptMedia(data.message.audioMessage.url, data.message.audioMessage.mimetype, data.key.id, data.message.audioMessage.mediaKey, 'audio');
   }
   if (data.message.imageMessage) {
     console.log("Image Message:", JSON.stringify(data.message.imageMessage, null, 2));
-    await downloadAndDecryptMedia(data.message.imageMessage.url, data.message.imageMessage.mimetype, data.key.id, data.message.imageMessage.mediaKey);
+    await downloadAndDecryptMedia(data.message.imageMessage.url, data.message.imageMessage.mimetype, data.key.id, data.message.imageMessage.mediaKey, 'image');
   }
   if (data.message.videoMessage) {
     console.log("Video Message:", JSON.stringify(data.message.videoMessage, null, 2));
-    await downloadAndDecryptMedia(data.message.videoMessage.url, data.message.videoMessage.mimetype, data.key.id, data.message.videoMessage.mediaKey);
+    await downloadAndDecryptMedia(data.message.videoMessage.url, data.message.videoMessage.mimetype, data.key.id, data.message.videoMessage.mediaKey, 'video');
   }
   if (data.message.documentMessage) {
     console.log("Document Message:", JSON.stringify(data.message.documentMessage, null, 2));
-    await downloadAndDecryptMedia(data.message.documentMessage.url, data.message.documentMessage.mimetype, data.key.id, data.message.documentMessage.mediaKey);
+    await downloadAndDecryptMedia(data.message.documentMessage.url, data.message.documentMessage.mimetype, data.key.id, data.message.documentMessage.mediaKey, 'document');
   }
   if (data.message.messageContextInfo) {
     console.log("Message Context Info:", JSON.stringify(data.message.messageContextInfo, null, 2));
